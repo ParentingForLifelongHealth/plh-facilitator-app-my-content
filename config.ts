a621ec8355b18a25f3e982b1c4e4aed185153abf
esmeetewinkel <74557272+esmeetewinkel@users.noreply.github.com>
2023-10-03 17:34:32 +0100

    Update config.ts - fix typo

--- config.ts
+++ config.ts
@@ -12,7 +12,7 @@ config.google_drive = {
 };
 
 config.git = {
-  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-zm-content.git",
+  content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-my-content.git",
   content_tag_latest: "1.1.16",
 };
 
