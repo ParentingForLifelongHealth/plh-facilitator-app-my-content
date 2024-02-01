7bb3c453d2bdc5d9bfa16d01cb051011f24a16f9
EsmeeIDEMS <esmee@idems.international>
2024-02-01 15:40:59 +0000

    content: 1.1.70

--- config.ts
+++ config.ts
@@ -20,7 +20,7 @@ config.android = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-my-content.git",
-  content_tag_latest: "1.1.69",
+  content_tag_latest: "1.1.70",
 };
 
 config.api.db_name = "plh_facilitator_my"
