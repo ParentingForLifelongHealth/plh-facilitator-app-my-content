0d855c695e64ebc8a8b525925ac605af70e0aed5
Johnny McQuade <jfmcquade@hotmail.com>
2024-08-06 14:57:58 +0100

    content: 1.1.81

--- config.ts
+++ config.ts
@@ -25,7 +25,7 @@ config.ios.app_name = "FaciNK";
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-my-content.git",
-  content_tag_latest: "1.1.80",
+  content_tag_latest: "1.1.81",
 };
 
 config.web.favicon_asset = "images/logos/favicon.png";
