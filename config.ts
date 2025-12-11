57d5c85c29a430e6f8bcdf02542eca52d653554d
GitHub Action <action@github.com>
2025-12-11 12:51:19 +0000

    Automated commit message

--- config.ts
+++ config.ts
@@ -33,7 +33,7 @@ config.ios.app_name = "FaciNK";
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-facilitator-app-my-content.git",
-  content_tag_latest: "2.2.32",
+  content_tag_latest: "2.2.33",
 };
 
 config.firebase = {
